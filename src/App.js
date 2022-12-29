11a5595975ee7f5947c3044d5fa3f20caffe7af8
Isaac Li <isaacli@mac.com>
2022-12-29 18:41:39 +0800

    version 1.0.3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import RootLayout from "./layouts/RootLayout"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import NotFound from "./NotFound"
 
 import HelpLayout from "./layouts/HelpLayout"
 import Faq from "./pages/help/Faq"
@@ -16,6 +17,7 @@ const router = createBrowserRouter(
         <Route path="faq" element={<Faq />} />
         <Route path="contact" element={<Contact />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 )
